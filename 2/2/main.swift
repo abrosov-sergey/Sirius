3bb41f7adcbe462ddfb9cf94a60a4b679193e2a7
Sergey Abrosov <sgabrosov@icloud.com>
2024-03-19 10:47:03 +0300

    Commited

---
+++ 2/2/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  2
+//
+//  Created by Sergey Abrosov on 19.03.2024.
+//
+
+import Foundation
+
+// 1) VCS - version control system
+
+// гит хранит файлы целиком
+
+// blob - длина файла + его содержимое
+// tree - хранит в себе блоб и сабдерево (subtree)
+
+// commit:
+//      - tree
+//      - parent
+//      - author
+//      - commiter
+
+untracked / tracked / staged / commited / modified 
+
+// 6) Регламент в работе
+
+//beb4cf4e7e8145c4b624afa9e3ee00beac704fb0
